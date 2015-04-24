cd1cbc4113b3b5d4c3f6184953559beed29f9b65
George Mitchell <george@lonelymountain.net>
2015-04-23 22:16:21 -0700

    More database layer changes
    Support for Genres and Songs.

--- CDCatalogDataModel/CDCatalogManager.cs
+++ CDCatalogDataModel/CDCatalogManager.cs
@@ -86,6 +86,163 @@ namespace CDCatalogDataModel
             }
 
         }
+
+        public static List<Genre> GetGenres()
+        {
+            using (CDCatalogEntities db = new CDCatalogEntities())
+            {
+                List<Genre> genreList = new List<Genre>();
+                try
+                {
+                    genreList = db.Genres.OrderBy(g => g.GenreName).ToList();
+                }
+                catch
+                {
+                    //TODO: Figure out what to do with an exception. Business logic should handle the return of an empty List<>.
+                }
+                return genreList;
+            }
+        }
+
+        public static bool AddGenre(Genre newGenre)
+        {
+            bool success = false;
+            using (CDCatalogEntities db = new CDCatalogEntities())
+            {
+                try
+                {
+                    db.Genres.Add(newGenre);
+                    db.SaveChanges();
+                    success = true;
+                }
+                catch
+                {
+                    //TODO: Figure out what to do with an exception. 
+                }
+                return success;
+            }
+        }
+
+        public static bool DeleteGenre(Genre genreToDelete)
+        {
+            bool success = false;
+            using (CDCatalogEntities db = new CDCatalogEntities())
+            {
+                try
+                {
+                    db.Genres.Remove(genreToDelete);
+                    db.SaveChanges();
+                    success = true;
+                }
+                catch
+                {
+                    //TODO: Figure out what to do with an exception. 
+                }
+                return success;
+            }
+
+        }
+
+        public static bool UpdateGenre(Genre editedGenre)
+        {
+            bool success = false;
+            using (CDCatalogEntities db = new CDCatalogEntities())
+            {
+                List<Genre> genreList = new List<Genre>();
+                try
+                {
+                    genreList = db.Genres.Where(g => g.GenreID == editedGenre.GenreID).ToList();
+                    genreList[0] = editedGenre;
+                    db.SaveChanges();
+                    success = true;
+                }
+                catch
+                {
+                    //TODO: Figure out what to do with an exception. 
+                }
+                return success;
+            }
+
+        }
+
+        public static List<Song> GetSongs()
+        {
+            using (CDCatalogEntities db = new CDCatalogEntities())
+            {
+                List<Song> songList = new List<Song>();
+                try
+                {
+                    songList = db.Songs.OrderBy(s => s.SongTitle).ToList();
+                }
+                catch
+                {
+                    //TODO: Figure out what to do with an exception. Business logic should handle the return of an empty List<>.
+                }
+                return songList;
+            }
+        }
+
+        public static bool AddSong(Song newSong)
+        {
+            bool success = false;
+            using (CDCatalogEntities db = new CDCatalogEntities())
+            {
+                try
+                {
+                    db.Songs.Add(newSong);
+                    db.SaveChanges();
+                    success = true;
+                }
+                catch
+                {
+                    //TODO: Figure out what to do with an exception. 
+                }
+                return success;
+            }
+        }
+
+        public static bool DeleteSong(Song songToDelete)
+        {
+            bool success = false;
+            using (CDCatalogEntities db = new CDCatalogEntities())
+            {
+                try
+                {
+                    db.Songs.Remove(songToDelete);
+                    db.SaveChanges();
+                    success = true;
+                }
+                catch
+                {
+                    //TODO: Figure out what to do with an exception. 
+                }
+                return success;
+            }
+
+        }
+
+        public static bool UpdateSong(Song editedSong)
+        {
+            bool success = false;
+            using (CDCatalogEntities db = new CDCatalogEntities())
+            {
+                List<Song> songList = new List<Song>();
+                try
+                {
+                    songList = db.Songs.Where(s => s.SongID == editedSong.SongID).ToList();
+                    songList[0] = editedSong;
+                    db.SaveChanges();
+                    success = true;
+                }
+                catch
+                {
+                    //TODO: Figure out what to do with an exception. 
+                }
+                return success;
+            }
+
+        }
+
     }
 
 }
