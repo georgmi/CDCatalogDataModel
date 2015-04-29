7b61bdf0c9183dfb42d26931bb1003ed6b29e33a
George Mitchell <george@lonelymountain.net>
2015-04-29 12:22:33 -0700

    Test cases that fail

--- CDCatalogTester/CDCatalogTester.cs
+++ CDCatalogTester/CDCatalogTester.cs
@@ -18,21 +18,46 @@ namespace CDCatalogTester
         List<Playlist> playlistList = new List<Playlist>();
         List<Song> songList = new List<Song>();
 
+        Artist testArtist = new Artist();
+        Genre testGenre = new Genre();
+        Album testAlbum = new Album();
+        Song testSong = new Song();
+
         [SetUp]
         public void Initialize()
         {
-            Artist testArtist = new Artist();
-            Genre testGenre = new Genre();
-            Album testAlbum = new Album();
-            Song testSong = new Song();
+            testArtist.ArtistName = "Test and the Artists";
+
+            testGenre.GenreName = "Test Genre";
+
+            testAlbum.AlbumTitle = "Songs to Test By";
+            //testAlbum.ArtistID = 2; TODO: Fix this
+            testAlbum.Year = 1990;
+
+            testSong.SongTitle = "I'll Have a Blue Testmas Without You";
+            testSong.TrackNumber = 1;
+            testSong.TrackLength = 325.0;
+            //testSong.AlbumID = 2; TODO: Fix this
+            //testSong.ArtistID = 2; TODO: Fix this
+            //testSong.GenreID = 2; TODO: Fix this
         }
 
+        [Test]
+        public void AddAnArtist()
+        {
+            bool result = false;
+            result = CDCatalogManager.AddArtist(testArtist);
+
+            Assert.IsTrue(result);
+        }
+        
         [Test]
         public void CheckArtists()
         {
             artistList = CDCatalogManager.GetArtists();
 
             Assert.IsNotEmpty(artistList);
+            Assert.Contains(testArtist, artistList);
         }
 
         [Test]
@@ -67,5 +92,29 @@ namespace CDCatalogTester
             Assert.IsNotEmpty(songList);
         }
     
+        [Test]
+        public void DeleteAnArtist()
+        {
+            bool result = false;
+            artistList = CDCatalogManager.GetArtists();
+
+            foreach(Artist a in artistList)
+            {
+                if(a.ArtistName == testArtist.ArtistName)
+                {
+                    result = CDCatalogManager.DeleteArtist(a);
+                }
+            }
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsArtistDeleted()
+        {
+            artistList = CDCatalogManager.GetArtists();
+
+            Assert.That(artistList, Has.No.Member(testArtist));
+        }
     }
 }
