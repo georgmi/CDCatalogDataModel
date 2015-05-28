79b7da96084d6766dfa233fa0f1c6a8ae65260da
George Mitchell <george@lonelymountain.net>
2015-05-28 11:18:59 -0700

    Playlist generation using combinatorial recursion
    Added a check to make sure there are enough songs to fill the target
    duration. Added a break out of the recursion after finding the first
    valid playlist. Now performs similarly to the prior, random method.

--- CDCatalogDataModel/CDCatalogProcess.cs
+++ CDCatalogDataModel/CDCatalogProcess.cs
@@ -18,146 +18,39 @@ namespace CDCatalogDataModel
             playlist.PlaylistName = playListName; //TODO: Consider checking for and preventing duplicate names.
             CDCatalogManager.AddPlaylist(playlist);
 
-            //playlistSongList = CombinationsPlaylist(workingList, playlist, seconds); //Hoo boy, does this take a long time.
-            playlistSongList = RandomPlayList(workingList, seconds, playlist);
+            playlistSongList = CombinationsPlaylist(workingList, playlist, seconds); //Works speedily now.
+            //playlistSongList = RandomPlayList(workingList, seconds, playlist);  //My prior algorithm. 
 
-            return playlistSongList;
-        }
-
-        public static List<PlaylistSong> RandomPlayList(List<Song> sourceList, int targetSeconds, Playlist playList)
-        {
-            ///Algorithm needs to:
-            ///1) Check playlist length against target--while totalLength less than or equal to targetSeconds + 60?
-            ///     
-            ///2) Reduce available songs to those that will fit in the time.
-            ///3) Add a song, add song length to totalLength
-            ///
-            Random rand = new Random();
-            List<PlaylistSong> playlistSongList = new List<PlaylistSong>();
-            PlaylistSong newPLS = new PlaylistSong();
-
-            bool keepGoing = true;
-            bool isFull = false;
-            bool notEnoughSongs = false;
-            int iterator = 0;
-            int index = 0;
-            int songOrder = 1;
-            int totalLength = 0;
-
-            List<Song> songList = sourceList.OrderBy(s => s.SongID).ToList();
-            List<Song> filteredList = songList.Where(s => s.TrackLength <= ((targetSeconds + 60) - totalLength)).ToList();
-            List<Song> workingList = new List<Song>();
-
-            //checking the iterator keeps this from becoming an infinite loop, 
-            //but all testing, even with fairly small datasets, has so far found
-            //a solution before the iterator iterates out.
-            while (iterator < 1000000000 && keepGoing == true) 
-            {
-                while (totalLength <= (targetSeconds + 60) && isFull == false && notEnoughSongs == false) 
-                {
-                    //reduce the available songs to only those which will not exceed the specified playlist length plus one minute.
-                    filteredList = songList.Where(s => s.TrackLength <= ((targetSeconds + 60) - totalLength)).ToList();
-
-                    if (filteredList.Count > 0) //If we're out of songs, we can't add any more to the playlist
-                    {
-                        index = rand.Next(0, filteredList.Count);
-                        workingList.Add(songList[index]);
-                        totalLength += songList[index].TrackLength;
-                        songList.RemoveAt(index);
-                    }
-                    else
-                    {
-                        notEnoughSongs = true;
-                        keepGoing = false;
-                    }
-
-                    if(totalLength >= (targetSeconds - 60))
-                    {
-                        isFull = true;
-                    }
-                }
-
-                if(totalLength >= (targetSeconds - 60) && totalLength <= (targetSeconds + 60))
-                {
-                    songOrder = 1;
-                    Song s = new Song();
-
-                    while(workingList.Count > 0)
-                    {
-                        newPLS = new PlaylistSong();
-                        s = workingList[rand.Next(0, workingList.Count)];
-                        newPLS.PlaylistID = playList.PlaylistID;
-                        newPLS.SongID = s.SongID;
-                        newPLS.SongOrder = songOrder;
-                        CDCatalogManager.AddPlaylistSong(newPLS);
-                        playlistSongList.Add(newPLS);
-                        workingList.Remove(s);
-                        songOrder++;
-                    }
-
-                    keepGoing = false;
-
-                }
-                else
-                {
-                    workingList.Clear();
-                    totalLength = 0;
-                    isFull = false;
-                    songList = sourceList.OrderBy(s => s.SongID).ToList();
-                    filteredList = songList.Where(s => s.TrackLength <= ((targetSeconds + 60) - totalLength)).ToList();
-                    iterator++;
-                }
-            }
-           
             return playlistSongList;
         }
 
         public static List<PlaylistSong> CombinationsPlaylist(List<Song> inputList, Playlist playlist, int durationSeconds)
         {
             Random rand = new Random();
-            List<List<Song>> allCombos = new List<List<Song>>();
-            //List<List<Song>> reducedCombos = new List<List<Song>>();
+            List<Song> resultList = new List<Song>();
             List<Song> workingList = Shuffle(inputList);
-            List<Song> selectedList = new List<Song>();
+            //List<Song> selectedList = new List<Song>(); //The algorithm now returns a single List<Song>; no need to pick one of several
             List<PlaylistSong> playlistSongList = new List<PlaylistSong>();
             PlaylistSong newPLS = null;
             int songOrder = 1;
 
-            allCombos = Combinations(workingList, durationSeconds);
+            resultList = Combinations(workingList, durationSeconds);
 
-            if(allCombos.Count > 0)
+            //A PlaylistSong is not the same as a Song; a PlaylistSong is just a pair of foreign keys (SongID, PlaylistID)
+            //plus an ordinal. In order to complete the Playlist, the PlaylistSongs need to be created from the ListSong<>.
+            foreach (Song s in resultList)
             {
-                //pick a valid playlist at random. This works around the tendency of the 
-                //Combinations method to find valid playlists with fewer songs before ones
-                //with more songs (which translates to a bias for longer songs).
-                int index = rand.Next(0, allCombos.Count);
-                selectedList = allCombos[index];
-
-                foreach(Song s in selectedList)
-                {
-                    newPLS = new PlaylistSong();
-                    newPLS.PlaylistID = playlist.PlaylistID;
-                    newPLS.SongID = s.SongID;
-                    newPLS.SongOrder = songOrder;
-                    CDCatalogManager.AddPlaylistSong(newPLS);
-                    playlistSongList.Add(newPLS);
-                    songOrder++;
-                }
-
+                newPLS = new PlaylistSong();
+                newPLS.PlaylistID = playlist.PlaylistID;
+                newPLS.SongID = s.SongID;
+                newPLS.SongOrder = songOrder;
+                CDCatalogManager.AddPlaylistSong(newPLS);
+                playlistSongList.Add(newPLS);
+                songOrder++;
             }
-            else
-            {
-                foreach(Song s in workingList)
-                {
-                    newPLS = new PlaylistSong();
-                    newPLS.PlaylistID = playlist.PlaylistID;
-                    newPLS.SongID = s.SongID;
-                    newPLS.SongOrder = songOrder;
-                    CDCatalogManager.AddPlaylistSong(newPLS);
-                    playlistSongList.Add(newPLS);
-                    songOrder++;
-                }
-            }
+
+            //The code block that was here to handle the case where no valid List<Song> was found
+            //is no longer necessary, because the recursion is now guaranteed to return a List<Song>.
 
             return playlistSongList;
         }
@@ -185,26 +78,41 @@ namespace CDCatalogDataModel
             return shuffledList;
         }
 
-        public static List<List<Song>> Combinations(List<Song> inputList, int durationSeconds)
+        public static List<Song> Combinations(List<Song> inputList, int durationSeconds)
         {
-            List<List<Song>> combinationsList = new List<List<Song>>();
-            Song[] workingList = new Song[inputList.Count + 1];
+            int totalDuration = 0;
+            foreach(Song s in inputList)
+            {
+                totalDuration += s.TrackLength;
+            }
+
+            //If there aren't enough songs to fill the specified playlist time, don't try.
+            //It takes a LONG time for this algorithm to fail.
+            if (totalDuration <= (durationSeconds + 60))
+            {
+                return inputList;
+            }
+
+            List<Song> outputList = new List<Song>();
+            Song[] workingList = new Song[inputList.Count + 1]; 
             int count = inputList.Count;
 
-            recursiveCombine(inputList, workingList, durationSeconds, combinationsList, count, 0, 0);
+            outputList = recursiveCombine(inputList, workingList, durationSeconds, count, 0, 0);
             
-            return combinationsList;
+            return outputList;
         }
 
-        public static void recursiveCombine(List<Song> inputList, Song[] workingList, int durationSeconds, List<List<Song>> combinationsList, int count, int recursionLevel, int start)
+        public static List<Song> recursiveCombine(List<Song> inputList, Song[] workingList, int durationSeconds, int count, int recursionLevel, int start)
         {
             List<Song> storageList = new List<Song>();
-
+            
             for (int i = start; i < count; i++)
             {
                 int totalDuration = 0;
                 workingList[recursionLevel] = inputList[i];
                 workingList[recursionLevel + 1] = null;
+                //Because we're re-using the same array for everything, we need to
+                //indicate to the for loop where the current combination stops.
                 storageList = new List<Song>();
                 for (int j = 0; j < recursionLevel; j++)
                 {
@@ -220,15 +128,123 @@ namespace CDCatalogDataModel
                 }
                 if (totalDuration >= (durationSeconds - 60) && totalDuration <= (durationSeconds + 60))
                 {
-                    combinationsList.Add(storageList);
-                    return;
+                    return storageList;
                 }
-
-                if (i < (count - 1))
+                else if (i < (count - 1) && totalDuration < (durationSeconds - 60))
                 {
-                    recursiveCombine(inputList, workingList, durationSeconds, combinationsList, count, (recursionLevel + 1), (i + 1));
+                    //I'm no longer passing a List<List<Song>> through the recursion, so now I need a way to percolate my 
+                    //results back up to the top. To do that, I need to know whether what's coming back is appropriate 
+                    //to pass up the stack, or if instead I need to keep going.
+                    storageList = recursiveCombine(inputList, workingList, durationSeconds, count, (recursionLevel + 1), (i + 1));
+                    totalDuration = 0;
+                    foreach(Song s in storageList)
+                    {
+                        if (null != s)
+                        {
+                            totalDuration += s.TrackLength;
+                        }
+                    }
+                    if (totalDuration >= (durationSeconds - 60) && totalDuration <= (durationSeconds + 60))
+                    {
+                        return storageList;
+                    }
                 }
             }
+
+            //If all else fails, return the original list of songs.
+            //HOPEFULLY, the only time we'll hit this is if the input song list is so short that it'll
+            //be quick to bottom out on the recursion.
+            return workingList.ToList();  
+            
+        }
+
+        //No longer using this code. 
+        public static List<PlaylistSong> RandomPlayList(List<Song> sourceList, int targetSeconds, Playlist playList)
+        {
+            ///Algorithm needs to:
+            ///1) Check playlist length against target--while totalLength less than or equal to targetSeconds + 60?
+            ///     
+            ///2) Reduce available songs to those that will fit in the time.
+            ///3) Add a song, add song length to totalLength
+            ///
+            Random rand = new Random();
+            List<PlaylistSong> playlistSongList = new List<PlaylistSong>();
+            PlaylistSong newPLS = new PlaylistSong();
+
+            bool keepGoing = true;
+            bool isFull = false;
+            bool notEnoughSongs = false;
+            int iterator = 0;
+            int index = 0;
+            int songOrder = 1;
+            int totalLength = 0;
+
+            List<Song> songList = sourceList.OrderBy(s => s.SongID).ToList();
+            List<Song> filteredList = songList.Where(s => s.TrackLength <= ((targetSeconds + 60) - totalLength)).ToList();
+            List<Song> workingList = new List<Song>();
+
+            //checking the iterator keeps this from becoming an infinite loop, 
+            //but all testing, even with fairly small datasets, has so far found
+            //a solution before the iterator iterates out.
+            while (iterator < 1000000000 && keepGoing == true)
+            {
+                while (totalLength <= (targetSeconds + 60) && isFull == false && notEnoughSongs == false)
+                {
+                    //reduce the available songs to only those which will not exceed the specified playlist length plus one minute.
+                    filteredList = songList.Where(s => s.TrackLength <= ((targetSeconds + 60) - totalLength)).ToList();
+
+                    if (filteredList.Count > 0) //If we're out of songs, we can't add any more to the playlist
+                    {
+                        index = rand.Next(0, filteredList.Count);
+                        workingList.Add(songList[index]);
+                        totalLength += songList[index].TrackLength;
+                        songList.RemoveAt(index);
+                    }
+                    else
+                    {
+                        notEnoughSongs = true;
+                        keepGoing = false;
+                    }
+
+                    if (totalLength >= (targetSeconds - 60))
+                    {
+                        isFull = true;
+                    }
+                }
+
+                if (totalLength >= (targetSeconds - 60) && totalLength <= (targetSeconds + 60))
+                {
+                    songOrder = 1;
+                    Song s = new Song();
+
+                    while (workingList.Count > 0)
+                    {
+                        newPLS = new PlaylistSong();
+                        s = workingList[rand.Next(0, workingList.Count)];
+                        newPLS.PlaylistID = playList.PlaylistID;
+                        newPLS.SongID = s.SongID;
+                        newPLS.SongOrder = songOrder;
+                        CDCatalogManager.AddPlaylistSong(newPLS);
+                        playlistSongList.Add(newPLS);
+                        workingList.Remove(s);
+                        songOrder++;
+                    }
+
+                    keepGoing = false;
+
+                }
+                else
+                {
+                    workingList.Clear();
+                    totalLength = 0;
+                    isFull = false;
+                    songList = sourceList.OrderBy(s => s.SongID).ToList();
+                    filteredList = songList.Where(s => s.TrackLength <= ((targetSeconds + 60) - totalLength)).ToList();
+                    iterator++;
+                }
+            }
+
+            return playlistSongList;
         }
 
     }
